cbb677e10abdc6311ce8ce182adddbf1f62d585e
HARADA Makoto <haramako@gmail.com>
2022-04-28 00:43:34 +0900

    Change.

--- src/mrb_unity.c
+++ src/mrb_unity.c
@@ -4,12 +4,11 @@
 #include <stdio.h>
 #include <memory.h>
 
-typedef void (*mrb_unity_abort_delegate_t)(const char*);
+typedef void (*mrb_unity_abort_delegate_t)(const char *);
 
 static mrb_unity_abort_delegate_t mrb_unity_abort_delegate;
 
-void
-mrb_unity_abort_callback(mrb_state *mrb, mrb_value exc)
+void mrb_unity_abort_callback(mrb_state *mrb, mrb_value exc)
 {
     mrb_value msg = mrb_funcall(mrb, exc, "to_s", 0);
     mrb_unity_abort_delegate(RSTRING_PTR(msg));
@@ -66,7 +65,7 @@ mrb_unity_bool_value(mrb_bool v)
 }
 
 // Value conversion.
-MRB_API mrb_int 
+MRB_API mrb_int
 mrb_unity_as_int(mrb_state *mrb, mrb_value obj)
 {
     return mrb_as_int(mrb, obj);
@@ -84,34 +83,53 @@ mrb_unity_string_buf(mrb_state *mrb, mrb_value obj, char *buf, size_t buf_len)
 {
     mrb_ensure_string_type(mrb, obj);
     size_t len = RSTRING_LEN(obj);
-    if( len > buf_len ){
+    if (len > buf_len)
+    {
         return -1;
-    }else{
+    }
+    else
+    {
         memcpy(buf, RSTRING_PTR(obj), len);
         return len;
     }
 }
 
-MRB_API mrb_value 
+MRB_API mrb_value
 mrb_unity_ptr_to_str(mrb_state *mrb, const char *p)
 {
     return mrb_ptr_to_str(mrb, p);
 }
 
+MRB_API mrb_bool
+mrb_unity_nil_p(mrb_value obj)
+{
+    return mrb_nil_p(obj);
+}
 
 // Other
-MRB_API mrb_value mrb_unity_funcall_argv(mrb_state *mrb, mrb_value val, const char *name, mrb_int argc,const mrb_value *args)
+MRB_API mrb_value
+mrb_unity_funcall_argv(mrb_state *mrb, mrb_value val, const char *name, mrb_int argc, const mrb_value *args)
 {
     mrb_sym name_sym = mrb_intern_cstr(mrb, name);
     return mrb_funcall_argv(mrb, val, name_sym, argc, args);
 }
 
-void
-mrb_mruby_unity_gem_init(mrb_state* mrb)
+MRB_API mrb_value
+mrb_unity_obj_value(void *p)
+{
+    return mrb_obj_value(p);
+}
+
+MRB_API mrb_value
+mrb_unity_mrb_state_exc(mrb_state *mrb)
+{
+    return mrb_obj_value(mrb->exc);
+}
+
+void mrb_mruby_unity_gem_init(mrb_state *mrb)
 {
 }
 
-void
-mrb_mruby_unity_gem_final(mrb_state* mrb)
+void mrb_mruby_unity_gem_final(mrb_state *mrb)
 {
 }
